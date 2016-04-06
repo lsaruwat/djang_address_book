22cd59935010bbc9beb8a3bf4e54baab6c4e5789
Logan Saruwatari <lsaruwatari@gmail.com>
2016-04-05 20:05:41 -0600

    Hello world is runnning. I had the route configured incorrectly. The main urls.py needs to reference the application urls.py file

--- address_book/app1/urls.py
+++ address_book/app1/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from . imort views
+
+from . import views
 
 urlpatterns = [
-	url(r'^admin/', views.index, name='index'),
+	url(r'^$', views.index, name='index'),
 ]
