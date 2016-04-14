6eebfb5259da50cb884d99aa6124e2881dfb4455
Zack LaVergne <zlavergne94@gmail.com>
2016-04-13 21:48:55 -0600

    applied the template to all the views

--- address_book/app1/views.py
+++ address_book/app1/views.py
@@ -4,62 +4,94 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	global header
-	user = "Logan"
-	html = """<h1>This is a website. Hello %s</h1>
-	<script type="text/javascript">
-	window.addEventListener("load", colorStuff, false);
+    global header
+    user = "Logan"
+    html = """<h1>This is a website. Hello %s</h1>
+    <script type="text/javascript">
+    window.addEventListener("load", colorStuff, false);
 
-	function colorStuff(){
-		setInterval( function(){
-			r = Math.floor(Math.random() * 255);
-			g = Math.floor(Math.random() * 255);
-			b = Math.floor(Math.random() * 255);
-			var h1s = document.getElementsByTagName("h1");
-			h1s[0].setAttribute("style", "color: rgb("+r+","+g+","+b+"); font-size: 100px;");
-			},1000);
-	}
+    function colorStuff(){
+        setInterval( function(){
+            r = Math.floor(Math.random() * 255);
+            g = Math.floor(Math.random() * 255);
+            b = Math.floor(Math.random() * 255);
+            var h1s = document.getElementsByTagName("h1");
+            h1s[0].setAttribute("style", "color: rgb("+r+","+g+","+b+"); font-size: 100px;");
+            },1000);
+    }
 
-	</script>
-	"""%(user)
-	return HttpResponse(html)
+    </script>
+    """%(user)
+    return HttpResponse(html)
 
 def home(request):
-	global header
-	user = "Logan"
-	html = "<h1>Welcome to your address book %s</h1>"%user
+    global header
+    user = "Logan"
+    html = "<h1>Welcome to your address book %s</h1>"%user
 
-	return HttpResponse(html)
+    return HttpResponse(html)
 
 def insertUser(request, user=None):
-	html= "insert user here"
+    #html= "insert user here"
+    errors = []
+    user = "Zack"
+    data = {
+        'heading': 'Insert a New User',
+        'content': 'Fill in the following information',
+        'user' : user,
+        'errors': errors,
+    }
 
-	return HttpResponse(html)
+    return render(request, 'app1/template.html', data)
 
 def deleteUser(request, user=None):
-	html= "delete user here"
+    #html= "delete user here"
+    errors = []
 
-	return HttpResponse(html)
+    data = {
+        'heading': 'Delete a User',
+        'content': 'Chooses which User to Delete',
+        'user' : user,
+        'errors': errors,
+    }
+
+    return render(request, 'app1/template.html', data)
 
 def updateUser(request, user=None):
-	html= "update user here"
+    #html= "update user here"
+    errors = []
 
-	return HttpResponse(html)
+    data = {
+        'heading': 'Update User Information',
+        'content': 'Fill in the following UPDATED information',
+        'user' : user,
+        'errors': errors,
+    }
+
+    return render(request, 'app1/template.html', data)
 
 def searchUser(request, user=None):
-	html= "search for users here"
+    #html= "search for users here"
+    errors = []
 
-	return HttpResponse(html)
+    data = {
+        'heading': 'Search for a User',
+        'content': 'Fill in the following information',
+        'user' : user,
+        'errors': errors,
+    }
+
+    return render(request, 'app1/template.html', data)
 
 def template(request):
-	errors = []
+    errors = []
 
-	data = {
-		'heading': 'Add New Student Grade',
-		'content': 'Fill in the following information',
-		'errors': errors,
-	}
+    data = {
+        'heading': 'Add New Student Grade',
+        'content': 'Fill in the following information',
+        'errors': errors,
+    }
 
-	return render(request, 'app1/template.html', data)
+    return render(request, 'app1/template.html', data)
 
 
